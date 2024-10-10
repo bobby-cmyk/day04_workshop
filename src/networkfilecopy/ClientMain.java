57b499c814c4a53c7eb0ab87e9de146f4a8739b0
Aiken Ong <aiken.ong@u.nus.edu>
2024-10-10 15:06:31 +0800

    Working version

--- src/networkfilecopy/ClientMain.java
+++ src/networkfilecopy/ClientMain.java
@@ -1,8 +1,12 @@
 package networkfilecopy;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
@@ -22,14 +26,23 @@ public class ClientMain {
         // Get the file path by getting the user to input through the command-line
         String filePath = args[0];
 
+        // Instantiate file 
+        File file = new File(filePath);
+        
         // Read the find
-        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(filePath));
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
 
-        // Intialise an arraylist to store the bytes of the file that wil be read
-        ArrayList<Integer> fileData = new ArrayList<>(); 
+        // Get file name
+        String fileName = file.getName();
+
+        // Get file size
+        long fileSize = file.length();
+
+        // Instantiate array with file size;
+        byte[] fileBytes = new byte[(int) fileSize];
 
         // Loop through the bytes of the file that will be read
-        while (true) {
+        for (int i = 0; i < fileSize; i++) {
             int fileByte = bis.read();
 
             // When the buffered input stream returns -1, it means that there are no more characters
@@ -37,14 +50,22 @@ public class ClientMain {
                 break;
             }
             // Add the byte into the arraylist to store
-            fileData.add(fileByte);
+            fileBytes[i] = (byte) fileByte;
         }
         // Close the buffered input stream after the reading of the file is done
         bis.close();
+
+        OutputStream os = sock.getOutputStream();
+        DataOutputStream dos = new DataOutputStream(os);
         
+        // Clean up the filePath to only include the file name
+        dos.writeUTF(fileName);
 
+        dos.writeLong(fileSize);
 
-
+        for (byte i : fileBytes) {
+            dos.writeByte(i);
+        }
 
         sock.close();
     }
--- src/networkfilecopy/ServerMain.java
+++ src/networkfilecopy/ServerMain.java
@@ -2,6 +2,7 @@ package networkfilecopy;
 
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
 
 
 public class ServerMain {
@@ -21,6 +22,33 @@ public class ServerMain {
             Socket sock = server.accept();
 
             System.out.printf("Got a new connection.\n");
+
+
+            InputStream is = sock.getInputStream();
+
+            DataInputStream dis = new DataInputStream(is);
+
+            String fileName = dis.readUTF();
+
+            long fileSize = dis.readLong();
+
+            byte[] fileBytes = new byte[(int) fileSize];
+
+            for (int i = 0; i < fileSize; i++) {
+                byte fileByte = dis.readByte();
+                fileBytes[i] = fileByte;
+            }
+
+            System.out.printf("File Name: %s, File Size: %d\n", fileName, fileSize);
+
+            String fileCopyName = "copy" + fileName;
+            // Read the find
+            BufferedOutputStream bis = new BufferedOutputStream(new FileOutputStream("servercopies/" + fileCopyName));
+
+            bis.write(fileBytes);
+
+            bis.flush();
+            bis.close();
 
             sock.close();
         }
